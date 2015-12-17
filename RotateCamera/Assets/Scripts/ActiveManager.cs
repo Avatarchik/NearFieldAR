f65ade46069f4c48d399f16846ccaa4a71e2daa9
Jagger Yu <tzuchieh928@gmail.com>
2015-12-17 19:24:20 +0800

    rotate virtual camera

---
+++ RotateCamera/Assets/Scripts/ActiveManager.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ActiveManager : MonoBehaviour {
+	private float angle;
+	private float speed = 0.5f;
+	public Transform TranslateLeft;
+	public Transform TranslateRight;
+	public Transform ScaleLeft;
+	public Transform ScaleRight;
+	public Transform RightPlane;
+	public Transform LeftView;
+	public Transform RightView;
+	public Transform VirtualObjectPos;
+	void Start () {
+	
+	}
+
+	//public float CurrentZoom;
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKey(KeyCode.JoystickButton9) ) 
+		{
+			ScaleLeft.localScale += new Vector3(0.04f, 0.04f, 0.04f);
+			ScaleRight.localScale += new Vector3(0.04f, 0.04f, 0.04f);
+			//leftCamera
+		}
+
+		if (Input.GetKey(KeyCode.JoystickButton8)) 
+		{
+			ScaleLeft.localScale -= new Vector3(0.04f, 0.04f, 0.04f);
+			ScaleRight.localScale -= new Vector3(0.04f, 0.04f, 0.04f);
+			//leftCamera
+		}
+
+		if (Input.GetKey (KeyCode.C)) 
+		{
+			TranslateLeft.localPosition += new Vector3(0.04f, 0.0f, 0.0f);
+			TranslateRight.localPosition -= new Vector3(0.04f, 0.0f, 0.0f);
+			//leftCamera
+		}
+		
+		if (Input.GetKey (KeyCode.V)) 
+		{
+			TranslateLeft.localPosition -= new Vector3(0.04f, 0.0f, 0.0f);
+			TranslateRight.localPosition += new Vector3(0.04f, 0.0f, 0.0f);
+			//leftCamera
+		}
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			angle += 200 * Time.deltaTime * speed;
+			LeftView.eulerAngles = new Vector3(0, angle, 0);
+			RightView.eulerAngles = new Vector3(0, -angle, 0);
+		}
+		
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			angle -= 200 * Time.deltaTime * speed;
+			LeftView.eulerAngles = new Vector3(0, angle, 0);
+			RightView.eulerAngles = new Vector3(0, -angle, 0);
+		}
+
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			RightPlane.localPosition += new Vector3(0.0f, 0.04f, 0.0f);
+
+		}
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			RightPlane.localPosition -= new Vector3(0.0f, 0.04f, 0.0f);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			VirtualObjectPos.localPosition += new Vector3(0.0f, 0.0f, 0.04f);
+		}
+		
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			VirtualObjectPos.localPosition -= new Vector3(0.0f, 0.0f, 0.04f);
+		}
+		
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			VirtualObjectPos.localPosition += new Vector3(0.0f, 0.04f, 0.0f);
+		}
+		
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			VirtualObjectPos.localPosition -= new Vector3(0.0f, 0.04f, 0.0f);
+		}
+	}
+}
---
+++ RotateCamera/Assets/Scripts/StartCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartCamera : MonoBehaviour {
+
+	public string deviceName = "UI325xLE-C_4102826019";
+	MeshRenderer mr;
+	// Use this for initialization
+	void Start () {
+
+        AVProLiveCameraManager.Instance.GetDevice(deviceName).Start(-1);    
+        mr = GetComponent<MeshRenderer> ();
+	}
+
+	private void UpdateCameras()
+	{
+		// Update all cameras
+		/*int numDevices = AVProLiveCameraManager.Instance.NumDevices;
+		//Debug.Log (numDevices);
+		for (int i = 0; i < numDevices; i++)
+		{
+			AVProLiveCameraDevice device = AVProLiveCameraManager.Instance.GetDevice(i);
+			
+			// Update the actual image
+			device.Update(false);
+		}*/
+
+        AVProLiveCameraDevice device = AVProLiveCameraManager.Instance.GetDevice(deviceName);
+		device.Update(false);
+        mr.material.mainTexture = AVProLiveCameraManager.Instance.GetDevice(deviceName).OutputTexture;
+	}
+	
+	private int _lastFrameCount;
+	void OnRenderObject()
+	{
+		if (_lastFrameCount != Time.frameCount)
+		{
+			_lastFrameCount = Time.frameCount;
+			
+			UpdateCameras();
+		}
+	}
+	
+/*	void Update () {
+		//Right Plane
+		//AVProLiveCameraManager.
+		mr.material.mainTexture = AVProLiveCameraManager.Instance.GetDevice (deviceNumber).OutputTexture;
+	}*/
+}
